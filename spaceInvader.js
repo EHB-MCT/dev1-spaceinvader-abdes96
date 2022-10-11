ac7851364551e472467c3c8a23fd56ce4e29fc41
Abdeslam Boutaarourt <abdeslam.boutaarourt@student.@ehb.be>
2022-10-11 23:08:24 +0200

    spaceinvader

--- spaceInvader.js
+++ spaceInvader.js
@@ -13,9 +13,40 @@ function Drawspaceinvader () {
 
 
     context.beginPath();
+    context.fillRect(50, 50, 300, 300);
     
-    context.rect(50, 50, 300, 300);
-    context.fillStyle = "BLACK";
+
+    context.beginPath();
+
+    context.fillStyle = "#66FF00";
+    context.rect(120, 300, 50, 50);
+
+    context.rect(225, 300, 50, 50);
+
+    context.rect(120, 250, 50, 50);
+    
+    context.rect(225, 250, 50, 50);
+
+    context.rect(75, 200, 50, 50);
+
+    context.rect(75, 150, 50, 50);
+
+    context.rect(75, 100, 50, 50);
+
+    context.rect(125, 100, 50, 50);
+    
+    context.rect(175, 100, 50, 50);
+    
+    context.rect(175, 150, 50, 50);
+    
+    context.rect(225, 100, 50, 50);
+
+    context.rect(275, 100, 50, 50);
+    
+    context.rect(275, 150, 50, 50);
+    
+    context.rect(275, 200, 50, 50);
+    
     context.fill();
 
     
