d358df44c2e5eef6ee3cf3df65fb30e03fac807a
Abdeslam Boutaarourt <abdeslam.boutaarourt@student.@ehb.be>
2022-10-11 23:23:17 +0200

    taak klaar ,efficienter code

--- spaceInvader.js
+++ spaceInvader.js
@@ -11,41 +11,39 @@ Drawspaceinvader();
 
 function Drawspaceinvader () {
 
-
     context.beginPath();
     context.fillRect(50, 50, 300, 300);
     
-
     context.beginPath();
 
     context.fillStyle = "#66FF00";
-    context.rect(120, 300, 50, 50);
+    context.rect(125, 275, 50, 50);
 
-    context.rect(225, 300, 50, 50);
+    context.rect(225, 275, 50, 50);
 
-    context.rect(120, 250, 50, 50);
+    context.rect(125, 225, 50, 50);
     
-    context.rect(225, 250, 50, 50);
+    context.rect(225, 225, 50, 50);
 
-    context.rect(75, 200, 50, 50);
+    context.rect(75, 175, 50, 50);
 
-    context.rect(75, 150, 50, 50);
+    context.rect(75, 125, 50, 50);
 
-    context.rect(75, 100, 50, 50);
+    context.rect(75, 75, 50, 50);
 
-    context.rect(125, 100, 50, 50);
+    context.rect(125, 75, 50, 50);
     
-    context.rect(175, 100, 50, 50);
+    context.rect(175, 75, 50, 50);
     
-    context.rect(175, 150, 50, 50);
+    context.rect(175, 125, 50, 50);
     
-    context.rect(225, 100, 50, 50);
+    context.rect(225, 75, 50, 50);
 
-    context.rect(275, 100, 50, 50);
+    context.rect(275, 75, 50, 50);
     
-    context.rect(275, 150, 50, 50);
+    context.rect(275, 125, 50, 50);
     
-    context.rect(275, 200, 50, 50);
+    context.rect(275, 175, 50, 50);
     
     context.fill();
 
