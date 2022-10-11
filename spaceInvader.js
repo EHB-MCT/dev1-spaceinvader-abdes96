b218470554d3729b9180a7271aef10b84a17b382
Abdeslam Boutaarourt <abdeslam.boutaarourt@student.@ehb.be>
2022-10-11 22:48:51 +0200

    achtergrond

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,22 @@
 "use strict";
 
-alert("Draw your space invader here");
+
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+Drawspaceinvader();
+
+
+function Drawspaceinvader () {
+
+
+    context.beginPath();
+    
+    context.rect(50, 50, 300, 300);
+    context.fillStyle = "BLACK";
+    context.fill();
+
+    
+}
